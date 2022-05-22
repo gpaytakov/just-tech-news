17509e29dd43f128473ce2e7e84f905026ebf52b
gpay <gpaytakov@gmail.com>
2022-05-22 15:44:17 -0500

    conditionally render the login links

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,11 @@
-const sequelize = require("../../config/connection");
 const router = require("express").Router();
-const { Post, User, Vote, Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+const { Post, User, Comment, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
-    order: [["created_at", "DESC"]],
     attributes: [
       "id",
       "post_url",
@@ -19,7 +19,6 @@ router.get("/", (req, res) => {
       ],
     ],
     include: [
-      // include the Comment model here:
       {
         model: Comment,
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
@@ -59,7 +58,6 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
-      // include the Comment model here:
       {
         model: Comment,
         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
@@ -89,27 +87,33 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Post.create({
-    title: req.body.title,
-    post_url: req.body.post_url,
-    user_id: req.body.user_id,
-  })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  if (req.session) {
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.session.user_id,
+    })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
-// PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  if (req.session) {
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put("/:id", (req, res) => {
@@ -137,6 +141,7 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
+  console.log("id", req.params.id);
   Post.destroy({
     where: {
       id: req.params.id,
